25bbaf4a0b2d96d7eaaf856fcd56421d1840bab3
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-03-30 09:29:21 -0400

    Pizza Q1

---
+++ cc-pizza.rb
@@ -0,0 +1,45 @@
+class Pizza
+
+  def initialize(toppings)
+    @toppings = toppings
+    @cooked = false
+    @cuts_remaining = 0
+    @slices = 0
+  end
+
+def bake
+  @cooked = true
+  @slices = 1
+  @cuts_remaining = 3
+end
+
+def cut
+  unless @cooked == true
+    puts "Error: Can't cut a raw pizza"
+    return
+  end
+
+  if @cuts_remaining == 3
+    @slices += 1
+    @cuts_remaining -= 1
+
+  elsif @cuts_remaining == 2 || @cuts_remaining == 1
+    @slices += 2
+    @cuts_remaining -= 1
+  elsif @cuts_remaining = 0
+    puts "ERROR: Pizza is too sliced"
+  end
+end
+
+def eat
+  unless @cooked == true
+    puts "ERROR: Can't eat raw pizza"
+    return
+  end
+
+  if @slices > 0
+    @slices -= 1
+    puts "nom"
+end
+
+end
